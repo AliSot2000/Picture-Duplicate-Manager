10bfd2288afeffd2f55faeda54f89b5ce2877610
AliSot2000 <alisot200@gmail.com>
2022-09-21 02:25:28 +0200

    + Need to parse shit and stuff, the main data interaction is still missing.

--- src/gui.py
+++ src/gui.py
@@ -22,7 +22,31 @@ class ScrollLabel(ScrollView):
         self.bind(on_scroll_stop=self.update_compare_pane)
 
     def update_compare_pane(self, *args,  **kwargs):
-        self.parent.parent.update_global(x=self.scroll_x, y=self.scroll_y)
+        self.parent.parent.update_scroll_metadata(x=self.scroll_x, y=self.scroll_y)
+
+
+class MetadataScrollLabel(ScrollView):
+    lbl = ObjectProperty(None)
+    text = StringProperty("example content")
+
+    def __init__(self, **kwargs):
+        super(MetadataScrollLabel, self).__init__(**kwargs)
+        self.bind(on_scroll_stop=self.update_compare_pane)
+
+    def update_compare_pane(self, *args,  **kwargs):
+        self.parent.parent.update_scroll_metadata(x=self.scroll_x, y=self.scroll_y)
+
+
+class PathScrollLabel(ScrollView):
+    lbl = ObjectProperty(None)
+    text = StringProperty("example content")
+
+    def __init__(self, **kwargs):
+        super(PathScrollLabel, self).__init__(**kwargs)
+        self.bind(on_scroll_stop=self.update_compare_pane)
+
+    def update_compare_pane(self, *args,  **kwargs):
+        self.parent.parent.update_scroll_path(x=self.scroll_x)
 
 
 class ComparePane(Widget):
@@ -33,7 +57,6 @@ class ComparePane(Widget):
     l_ofpath = ObjectProperty(None)
     l_naming_tag = ObjectProperty(None)
     l_new_name = ObjectProperty(None)
-    l_binary_comp = ObjectProperty(None)
     l_metadata = ObjectProperty(None)
 
     image_path = StringProperty("/home/alisot2000/Documents/06 ReposNCode/PictureMerger/test-images/IMG_2162.JPG")
@@ -44,14 +67,23 @@ class ComparePane(Widget):
     bin_comp = BooleanProperty(True)
     metadata = StringProperty("""Composite:ImageSize: 2048 1536\nComposite:Megapixels: 3.145728\nExifTool:ExifToolVersion: 12.44\nFile:Directory: /media/alisot2000/DumpStuff/Picture consolidation/Export Fotos Macbok archive 2/iptc/1. April 2018\nFile:FileAccessDate: 2022:09:12 18:34:46+02:00\nFile:FileInodeChangeDate: 2022:09:12 17:11:13+02:00\nFile:FileModifyDate: 2018:04:01 02:43:30+02:00\nFile:FileName: IMG_2130.PNG\nFile:FilePermissions: 100777\nFile:FileSize: 1912870\nFile:FileType: PNG\nFile:FileTypeExtension: PNG\nFile:MIMEType: image/png\nPNG:BitDepth: 8\nPNG:ColorType: 2\nPNG:Compression: 0\nPNG:Filter: 0\nPNG:ImageHeight: 1536\nPNG:ImageWidth: 2048\nPNG:Interlace: 0\nPNG:SRGBRendering: 0\nSourceFile: /media/alisot2000/DumpStuff/Picture consolidation/Export Fotos Macbok archive 2/iptc/1. April 2018/IMG_2130.PNG\nXMP:DateCreated: 2018:04:01 02:43:29\nXMP:UserComment: Screenshot\nXMP:XMPToolkit: XMP Core 5.4.0""")
 
-    def __init__(self, **kwargs):
-        super(ComparePane, self).__init__(**kwargs)
-
-    def update_global(self, *args, x: float, y: float,  **kwargs):
+    def update_scroll_metadata(self, *args, x: float, y: float, **kwargs):
         self.parent.update_scroll_meta(x=x, y=y)
+
+    def update_scroll_path(self, *args, x: float, **kwargs):
+        self.parent.update_scroll_path(x=x)
 
     def open_modal(self):
         self.parent.parent.parent.parent.open_modal(t_id=self)
+
+    def set_button_color(self):
+        if self.ids.dlt_btn.state == "down":
+            self.ids.dlt_btn.background_color = 1.0, 0.0, 0.0
+            self.ids.dlt_btn.text = "to trash"
+        else:
+            self.ids.dlt_btn.background_color = 0.2, 0.6, 0.2
+            self.ids.dlt_btn.text = "keep"
+
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
@@ -72,6 +104,11 @@ class MyGrid(GridLayout):
             c.l_metadata.scroll_x = x
             c.l_metadata.scroll_y = y
 
+    def update_scroll_path(self, *args, x: float, **kwargs):
+        for c in self.children:
+            c: ComparePane
+            c.l_ofpath.scroll_x = x
+
 
 class MyBox(BoxLayout):
     def __init__(self, **kwargs):
@@ -81,12 +118,24 @@ class MyBox(BoxLayout):
 
 class MyFloat(FloatLayout):
     compareGrid = ObjectProperty(None)
-    filenameModal = ObjectProperty(None)
+    filenameModal = None
+
+    dup_fp: str
+    def __init__(self, source_fp: str, **kwargs):
+        super(MyFloat, self).__init__(**kwargs)
+        self.dup_fp = source_fp
+
+    def load_(self):
+        pass
 
     def open_modal(self, t_id):
         fm = SetDateModal(target_id=t_id)
-        self.filenameModal = ObjectProperty(fm)
+        self.filenameModal = fm
         self.add_widget(fm)
+
+    def close_modal(self):
+        self.remove_widget(self.filenameModal)
+        self.filenameModal = None
 
 
 class CustomDateTag(TextInput):
@@ -98,7 +147,7 @@ class CustomDateTag(TextInput):
         self.parent.parent.text_content()
 
 
-class SetDateModal(ModalView):
+class SetDateModal(GridLayout):
     tagLabel = ObjectProperty(None)
     customDateTag = ObjectProperty(None)
     customDateTime = ObjectProperty(None)
@@ -130,10 +179,26 @@ class SetDateModal(ModalView):
             print("Set to true")
             print(self.customDateTimeInput.disabled)
 
+    def close(self):
+        """
+        Closes the modal by removing the widget from the parent
+        :return:
+        """
+        self.parent.close_modal()
+
+    def apply_close(self):
+        """
+        Applies the new name to the selected comparePane,
+        Closes the modal by removing the widget.
+        :return:
+        """
+        print("Need to update value in ComparePane")
+        self.parent.close_modal()
+
 
 class PictureLibrary(App):
     def build(self):
-        mf = MyFloat()
+        mf = MyFloat("asdf")
         # Clock.schedule_interval(mf.comparegrid.action, 2.0)
         return mf
 
