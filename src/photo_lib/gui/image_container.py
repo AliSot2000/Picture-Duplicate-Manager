370769e19082dafe8f078bbba616b7e456b6b036
AliSot2000 <alisot200@gmail.com>
2023-05-05 14:34:47 +0200

    + Aspect ratio as precomputed attribute

--- src/photo_lib/gui/image_container.py
+++ src/photo_lib/gui/image_container.py
@@ -6,11 +6,13 @@ class ResizingImage(QLabel):
     img_lbl: QLabel
     pxmap: QPixmap
     fpath: str
+    width_div_height: float
 
     def __init__(self, file_path: str):
         super().__init__()
         self.fpath = file_path
         self.pixmap = QPixmap(file_path)
+        self.width_div_height = self.pixmap.width() / self.pixmap.height()
         self.setPixmap(self.pixmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
         # self.media.setScaledContents(True) # Will scale the widget, undoing the keep aspect ratio.
         # self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)  # Image goes behind other widgets.
