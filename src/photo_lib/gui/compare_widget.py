a1aea21d9e40c11b3a060ff35a736073942d7756
AliSot2000 <alisot200@gmail.com>
2023-05-05 17:29:25 +0200

    + Passing synchronized scroll function to children + Implementend synchronized scroll function.

--- src/photo_lib/gui/compare_widget.py
+++ src/photo_lib/gui/compare_widget.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import QHBoxLayout, QWidget, QLabel, QPushButton
 from photo_lib.gui.model import Model
 from typing import List
 from photo_lib.gui.media_pane import MediaPane
+from photo_lib.gui.text_scroll_area import TextScroller
 
 
 class CompareRoot(QWidget):
@@ -29,7 +30,7 @@ class CompareRoot(QWidget):
 
         # Go through all DatabaseEntries, generate a MediaPane from each one and add the panes to the layout.
         for dbe in self.model.files:
-            pane = MediaPane(self.model, dbe)
+            pane = MediaPane(self.model, dbe, self.synchronized_scroll)
             self.media_panes.append(pane)
             self.layout.addWidget(pane)
             # pane.show()
@@ -46,4 +47,26 @@ class CompareRoot(QWidget):
 
         self.media_panes = []
 
+    def synchronized_scroll(self, name: str, caller: TextScroller, rx: float, ry: float):
+        """
+        Iterate over all Media panes in the compare view and set the scroll amount to the same relative value indicated
+        by rx and ry.
 
+        The TextScrollers that are going to be affected are indicated by the name argument which specifies the
+        attribute name of the TextScroller.
+
+        The caller is needed to prevent a recursive self call with no termination.
+
+        :param name: The name of the attribute to synchronize.
+        :param caller: The TextScroller that called this function.
+        :param rx: The relative x scroll value.
+        :param ry: The relative y scroll value.
+        """
+        for mp in self.media_panes:
+            try:
+                element = getattr(mp, name)
+            except AttributeError:
+                print("Share Scroll Bug: Couldn't find attribute to share scroll value with.")
+                exit(100)
+            if element != caller:
+                element.scroll_from_ratio(rx, ry)
