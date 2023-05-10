1ccb24a41f78746f8b1c125068b447a83ac9918f
AliSot2000 <alisot200@gmail.com>
2023-05-10 07:04:05 +0200

    + Implemented Synchronized Scroll Bar with TextScroller + Changed formatting

--- src/photo_lib/gui/media_pane.py
+++ src/photo_lib/gui/media_pane.py
@@ -1,12 +1,13 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QFrame
 from PyQt6.QtMultimedia import QMediaPlayer
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 
 from photo_lib.gui.image_container import ResizingImage
+from photo_lib.gui.text_scroll_area import TextScroller
 from photo_lib.gui.model import Model
 from photo_lib.PhotoDatabase import DatabaseEntry, PhotoDb
-from typing import Union
+from typing import Union, Callable
 
 
 # Rotate Image
@@ -18,6 +19,13 @@ from typing import Union
 #     pixmap = pixmap.transformed(rm);
 #     my_label->setPixmap(pixmap);
 # }
+
+def bake_attribute(name: str, func: Callable):
+    def baked_attribute_func(*args, **kwargs):
+        return func(*args, name=name,**kwargs)
+
+    return baked_attribute_func
+
 
 class MediaPane(QWidget):
     """
@@ -31,24 +39,27 @@ class MediaPane(QWidget):
     # Child Widgets, and associated attributes
     media: Union[ResizingImage, QMediaPlayer]
     pixmap: QPixmap = None
-    original_name_lbl: QLabel
-    original_path_lbl: QLabel
+    original_name_lbl: TextScroller
+    original_path_lbl: TextScroller
     tag_lbl: QLabel
     new_name_lbl: QLabel
     file_size_lbl: QLabel
     file_size: str = ""
-    metadata_lbl: QLabel
+    metadata_lbl: TextScroller
     metadata: str = ""
 
     main_button: QPushButton
     delete_button: QPushButton
     change_tag_button: QPushButton
 
-    min_width:int = 200
-    min_height:int = 600
+    min_width:int = 300
+    max_height:int = 540
 
-    def __init__(self, model: Model, entry: DatabaseEntry):
+    share_scroll: Callable
+
+    def __init__(self, model: Model, entry: DatabaseEntry, share_scroll: Callable):
         super().__init__()
+        self.share_scroll = share_scroll
         self.model = model
         self.dbe = entry
         self.layout = QVBoxLayout()
@@ -63,33 +74,52 @@ class MediaPane(QWidget):
         # Assuming default for the moment and just assuming that we get a picture.
         file_path = self.model.pdb.path_from_datetime(dt_obj=self.dbe.datetime, file_name=self.dbe.new_name)
         self.media = ResizingImage(file_path=file_path)
-        self.media.setFixedHeight(self.min_height)
-        self.media.setFixedWidth(self.min_width)
 
         # creating all the necessary labels
-        self.original_name_lbl = QLabel()
-        self.original_name_lbl.setText(self.dbe.org_fname)
-        self.original_name_lbl.setFixedHeight(20)
-        self.original_name_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        self.original_path_lbl = QLabel()
-        self.original_path_lbl.setText(self.dbe.org_fpath)
-        self.original_path_lbl.setFixedHeight(20)
-        self.original_path_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        self.original_name_lbl = TextScroller()
+        self.original_name_lbl.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.original_name_lbl.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.original_name_lbl.setFixedHeight(30)
+        self.original_name_lbl.setFrameShape(QFrame.Shape.NoFrame)
+        self.original_name_lbl.set_text(self.dbe.org_fname)
+        self.original_name_lbl.text_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        # Using Decorator to set the attribute fix and not having to mess around with the text scroller having to
+        # know its attribute name in the parent.
+        self.original_name_lbl.share_scroll = bake_attribute("original_name_lbl", self.share_scroll)
+
+        self.original_path_lbl = TextScroller()
+        self.original_path_lbl.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.original_path_lbl.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.original_path_lbl.setFixedHeight(30)
+        self.original_path_lbl.setFrameShape(QFrame.Shape.NoFrame)
+        self.original_path_lbl.set_text(self.dbe.org_fpath)
+        self.original_path_lbl.text_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        # Dito self.original_name_lbl
+        self.original_path_lbl.share_scroll = bake_attribute("original_path_lbl", self.share_scroll)
+
         self.tag_lbl = QLabel()
+        self.tag_lbl.setFixedHeight(30)
         self.tag_lbl.setText(self.dbe.naming_tag)
-        self.tag_lbl.setFixedHeight(20)
         self.tag_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+
         self.new_name_lbl = QLabel()
+        self.new_name_lbl.setFixedHeight(30)
         self.new_name_lbl.setText(self.dbe.new_name)
-        self.new_name_lbl.setFixedHeight(20)
         self.new_name_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+
         self.file_size_lbl = QLabel()
+        self.file_size_lbl.setFixedHeight(30)
         self.file_size_lbl.setText(self.file_size)
-        self.file_size_lbl.setFixedHeight(20)
         self.file_size_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        self.metadata_lbl = QLabel()
-        self.metadata_lbl.setText(self.metadata)
-        self.metadata_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+
+        self.metadata_lbl = TextScroller()
+        self.metadata_lbl.set_text(self.metadata)
+        self.metadata_lbl.setFrameShape(QFrame.Shape.NoFrame)
+        self.metadata_lbl.text_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        # self.metadata_lbl.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)
+        # Dito self.original_name_lbl
+        self.metadata_lbl.share_scroll = bake_attribute("metadata_lbl", self.share_scroll)
+
         # Adding all the widgets.
         self.layout.addWidget(self.media)
         self.layout.addWidget(self.original_name_lbl)
@@ -102,6 +132,7 @@ class MediaPane(QWidget):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         self.media.setFixedWidth(self.width())
+        self.media.setFixedHeight(min(self.max_height, int(self.width() / self.media.width_div_height)))
         # self.media.setScaledContents(True)
 
 
