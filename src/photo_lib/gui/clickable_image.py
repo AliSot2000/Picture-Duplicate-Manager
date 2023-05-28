516125801787745ae1a5686051e4830b8cb47d3a
AliSot2000 <alisot200@gmail.com>
2023-05-28 17:36:42 +0200

    + Moved setting pixmap and stuff to another function

--- src/photo_lib/gui/clickable_image.py
+++ src/photo_lib/gui/clickable_image.py
@@ -10,12 +10,9 @@ class ClickableImage(QPushButton):
 
     def __init__(self, file_path: str):
         super().__init__()
-        self.fpath = file_path
-        self.pixmap = QPixmap(file_path)
-        self.width_div_height = self.pixmap.width() / self.pixmap.height()
-        self.setIcon(QIcon(self.pixmap))
-        self.setIconSize(self.size())
-        self.setStyleSheet("border: none;")
+        self.pixmap = None
+        self.load_image(file_path)
+
         # self.setPixmap(self.pixmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
         # self.media.setScaledContents(True) # Will scale the widget, undoing the keep aspect ratio.
         # self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)  # Image goes behind other widgets.
@@ -26,5 +23,10 @@ class ClickableImage(QPushButton):
         # self.setPixmap(self.pixmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
         # self.media.setScaledContents(True)
 
-    def test_click(self):
-        print("Image clicked")
+    def load_image(self, file_path: str):
+        self.fpath = file_path
+        self.pixmap = QPixmap(file_path)
+        self.width_div_height = self.pixmap.width() / self.pixmap.height()
+        self.setIcon(QIcon(self.pixmap))
+        self.setIconSize(self.size())
+        self.setStyleSheet("border: none;")
