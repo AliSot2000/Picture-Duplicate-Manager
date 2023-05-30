2a4687e7224eba398a156374fd752a4e8319042e
AliSot2000 <alisot200@gmail.com>
2023-05-30 10:56:52 +0200

    + ResizableImage now clickable and works flawlessly.

--- src/photo_lib/gui/image_container.py
+++ src/photo_lib/gui/image_container.py
@@ -1,8 +1,8 @@
-from PyQt6.QtWidgets import QLabel, QSizePolicy
+from PyQt6.QtWidgets import QLabel, QSizePolicy, QPushButton, QVBoxLayout
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 
-class ResizingImage(QLabel):
+class ResizingImage(QPushButton):
     img_lbl: QLabel
     pxmap: QPixmap
     fpath: str
@@ -10,15 +10,21 @@ class ResizingImage(QLabel):
 
     def __init__(self, file_path: str):
         super().__init__()
-        self.fpath = file_path
-        self.pixmap = QPixmap(file_path)
-        self.width_div_height = self.pixmap.width() / self.pixmap.height()
-        self.setPixmap(self.pixmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
+        self.img_lbl = QLabel()
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.img_lbl)
         # self.media.setScaledContents(True) # Will scale the widget, undoing the keep aspect ratio.
-        # self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)  # Image goes behind other widgets.
+        self.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)  # Image goes behind other widgets.
+        self.load_image(file_path)
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
-        self.setPixmap(self.pixmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
+        if self.pxmap is not None:
+            self.img_lbl.setPixmap(self.pxmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
         # self.media.setScaledContents(True)
 
+    def load_image(self, file_path: str):
+        self.fpath = file_path
+        self.pxmap = QPixmap(file_path)
+        self.width_div_height = self.pxmap.width() / self.pxmap.height()
+        self.img_lbl.setPixmap(self.pxmap.scaled(self.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
