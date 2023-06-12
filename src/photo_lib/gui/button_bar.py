a49b04f7d1ce3471c1b7156b2f2bbbd395be6956
AliSot2000 <alisot200@gmail.com>
2023-06-12 02:44:29 +0200

    + QButtonbar switched to action buttons.

--- src/photo_lib/gui/button_bar.py
+++ src/photo_lib/gui/button_bar.py
@@ -1,25 +1,27 @@
-from PyQt6.QtWidgets import QPushButton, QHBoxLayout, QLabel
+from PyQt6.QtWidgets import QHBoxLayout, QLabel
+from photo_lib.gui.action_button import QActionButton
+
 
 class ButtonBar(QLabel):
 
-    next_button: QPushButton
-    commit_all: QPushButton
-    commit_selected: QPushButton
+    next_button: QActionButton
+    commit_all: QActionButton
+    commit_selected: QActionButton
     status: QLabel
 
     box_layout: QHBoxLayout
 
     def __init__(self):
         super().__init__()
-        self.next_button = QPushButton("Next")
+        self.next_button = QActionButton("Next")
         self.next_button.setToolTip("Move to next duplicate cluster in database")
         self.next_button.setMaximumWidth(150)
 
-        self.commit_all = QPushButton("Commit All")
+        self.commit_all = QActionButton("Commit All")
         self.commit_all.setToolTip("Set the entry marked as main as the original, remove all other entries from the database")
         self.commit_all.setMaximumWidth(150)
 
-        self.commit_selected = QPushButton("Commit Selected")
+        self.commit_selected = QActionButton("Commit Selected")
         self.commit_selected.setToolTip("Set the entry marked as main as the original, remove all as delete marked entries from the database")
         self.commit_selected.setMaximumWidth(150)
 
