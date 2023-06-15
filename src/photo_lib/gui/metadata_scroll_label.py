de3ca0b464cc4239934ff8a26dbfb42cd3da8be8
AliSot2000 <alisot200@gmail.com>
2023-06-15 23:17:21 +0200

    + Forgot the conditional builder.

--- src/photo_lib/gui/metadata_scroll_label.py
+++ src/photo_lib/gui/metadata_scroll_label.py
@@ -1,5 +1,13 @@
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.scrollview import ScrollView
+from kivy.lang import Builder
+import os
+
+
+metadata_scroll_label_loaded = False
+if not metadata_scroll_label_loaded:
+    Builder.load_file(os.path.join(os.path.dirname(__file__), "metadata_scroll_label.kv"))
+    metadata_scroll_label_loaded = True
 
 
 class MetadataScrollLabel(ScrollView):
