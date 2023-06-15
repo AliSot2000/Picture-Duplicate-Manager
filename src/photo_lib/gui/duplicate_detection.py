7746f7aafc45252ef4bc8e39608341654d9eb3a5
AliSot2000 <alisot200@gmail.com>
2023-06-15 21:53:03 +0200

    + Switched duplicate_detection.py to include guard style building

--- src/photo_lib/gui/duplicate_detection.py
+++ src/photo_lib/gui/duplicate_detection.py
@@ -6,11 +6,16 @@ from photo_lib.gui.progress_info import ProgressInfo
 from photo_lib.gui.root_widget_stub import RootWidgetStub
 
 
+duplicate_detection_loaded = False
+if not duplicate_detection_loaded:
+    Builder.load_file(os.path.join(os.path.dirname(__file__), "duplicate_detection.kv"))
+    duplicate_detection_loaded = True
+
+
 class DuplicateDetection(Popup):
     root: RootWidgetStub
 
     def __init__(self, root_wdg: RootWidgetStub, **kwargs):
-        Builder.load_file(os.path.join(os.path.dirname(__file__), "duplicate_detection.kv"))
         self.root = root_wdg
         super().__init__(**kwargs)
         self.progressbar = ProgressInfo(self.root.load_entry)
