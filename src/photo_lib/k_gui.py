3065322fe2673730a9cbacabdc560e579210ee98
AliSot2000 <alisot200@gmail.com>
2023-06-15 23:31:39 +0200

    + Moved MyGrid to different files

--- src/photo_lib/k_gui.py
+++ src/photo_lib/k_gui.py
@@ -43,39 +43,6 @@ from photo_lib.gui.scroll_label import ScrollLabel
 
 
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.bind(minimum_width=self.setter('width'))
-
-    def update_scroll_meta(self, *args, x: float, y: float, caller: MetadataScrollLabel, **kwargs):
-        """
-        Updates the scroll value of all meta_data labels to the same value
-        :param args: just for safety
-        :param x: new target x
-        :param y: new target y
-        :param kwargs: just for safety
-        :return:
-        """
-        for c in self.children:
-            c: ComparePane
-            if c.l_metadata == caller:
-                continue
-            c.l_metadata.scroll_x = x
-            c.l_metadata.scroll_y = y
-            c.l_metadata.last_scroll_x = x
-            c.l_metadata.last_scroll_y = y
-
-    def update_scroll_path(self, *args, x: float, caller: PathScrollLabel,  **kwargs):
-        for c in self.children:
-            c: ComparePane
-            if c == caller:
-                continue
-            c.l_ofpath.scroll_x = x
-            c.l_ofpath.last_scroll_x = x
-
-    def open_image_popup(self, path: str):
-        self.parent.parent.parent.open_image_popup(path)
 
 
 class FlexibleBox(BoxLayout):
