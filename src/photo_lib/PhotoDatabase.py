c91fc9fde8cf718e5dc26b913c91f6ea6f59fec7
AliSot2000 <alisot200@gmail.com>
2023-07-13 17:05:18 +0200

    + original_google_metadata added state -1 for not present.

--- src/photo_lib/PhotoDatabase.py
+++ src/photo_lib/PhotoDatabase.py
@@ -151,7 +151,7 @@ class PhotoDb:
          "present INTEGER DEFAULT 1 CHECK (present in (0, 1) ), "
          "verify INTEGER DEFAULT 0 CHECK (verify in (0, 1)),"
          "trashed INTEGER DEFAULT 0 CHECK (trash in (0, 1)),"
-         "original_google_metadata INTEGER DEFAULT 1 CHECK (original_google_metadata in (0, 1)),"
+         "original_google_metadata INTEGER DEFAULT -1 CHECK (original_google_metadata in (0, 1, -1)),"
          "timestamp INTEGER DEFAULT 0)")
 
     names_table_command: str = \
@@ -167,7 +167,7 @@ class PhotoDb:
          " datetime TEXT,"
          " successor INTEGER,"
          " former_name TEXT DEFAULT NULL,"
-         "original_google_metadata INTEGER DEFAULT 1 CHECK (original_google_metadata in (0, 1)),"
+         "original_google_metadata INTEGER DEFAULT -1 CHECK (original_google_metadata in (0, 1, -1)),"
          " FOREIGN KEY (successor) REFERENCES images(key))")
 
     import_tables_table_command: str = \
