5f92efa330848df49aa57d8b4ebf27cb88f2b98e
AliSot2000 <alisot200@gmail.com>
2023-08-12 16:45:49 +0200

    + Created base shared class for displaying images.

---
+++ src/photo_lib/gui/base_image.py
@@ -0,0 +1,122 @@
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtGui import QPixmap, QPainter, QFont
+from PyQt6.QtCore import Qt, QRect, QPoint, QSize, pyqtSignal
+import sys
+import os
+from typing import Union
+import warnings
+
+class BaseImage(QWidget):
+    pixmap = None
+
+    __file_path: str = None
+    width_div_height: float = 1.0
+
+    filler_color = Qt.GlobalColor.darkGray
+
+    def __init__(self, file_path: str = None):
+        super().__init__()
+        self.file_path = file_path
+
+    @property
+    def file_path(self):
+        """
+        Get the current file path of the image
+        :return:
+        """
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self, value: Union[None, str]):
+        """
+        Set the file path of the image. This if the image is supposed to be loaded it will be loaded as well.
+        The filepath can also be left empty to have a template.
+        :param value:
+        :return:
+        """
+        self.__file_path = value
+
+        if self.__file_path is not None:
+            self._load_image()
+
+    def _load_image(self):
+        """
+        Load the image from the file path into ram and set the pixmap
+        :return:
+        """
+        assert self.file_path is not None, "File path must be set before loading image."
+        self.pixmap = QPixmap(self.file_path)
+
+        if (ext := os.path.splitext(self.file_path)[1].lower()) not in [".png", ".jpg", ".jpeg", ".gif"]:
+            warnings.warn(f"File must be an image. File Extension: {ext}")
+        else:
+            try:
+                self.width_div_height = self.pixmap.width() / self.pixmap.height()
+            except ZeroDivisionError:
+                self.width_div_height = 1.0
+
+        self.updateGeometry()
+        if not self.pixmap.isNull() and self.isVisible():
+            self.update()
+
+    def _unload_image(self):
+        """
+        Removes the image from ram. The widget will draw a dark gray rectangle.
+        :return:
+        """
+        self.pixmap = None
+        self.updateGeometry()
+        if self.isVisible():
+            self.update()
+
+    def sizeHint(self):
+        """
+        Custom implementation of the size hint depending on if the image is loaded or not.
+        :return:
+        """
+        if self.pixmap and not self.pixmap.isNull():
+            return self.pixmap.size()
+        return QSize()
+
+    def paintEvent(self, event):
+        """
+        Custom implementation of the paint event to rescale the image to fit.
+        :param event:
+        :return:
+        """
+        if not self.pixmap or self.pixmap.isNull():
+            self.empty_pixmap_painter()
+            return
+
+        # Draw when image is successfully loaded n stuffl.
+        if self.size() == self.pixmap.size():
+            r = self.rect()
+        else:
+            r = QRect(QPoint(),
+                self.pixmap.size().scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio))
+            r.moveCenter(self.rect().center())
+        qp = QPainter(self)
+        qp.drawPixmap(r, self.pixmap)
+
+    def empty_pixmap_painter(self):
+        """
+        In case there's no image loaded, paint a message on the widget.
+        :return:
+        """
+        # Create a filler shape to indicate where the image is supposed to be.
+        pt = QPainter(self)
+        pt.fillRect(self.rect(), self.filler_color)
+
+        if self.pixmap is not None and self.pixmap.isNull():
+
+            # Draw the text in the middle of the widget
+            text = "Couldn't load file"
+            font = QFont("Arial", 12, QFont.Weight.Bold)
+            pt.setFont(font)
+            text_rect = pt.boundingRect(self.rect(), 0, text)
+            text_position = self.rect().center() - text_rect.center()
+            pt.drawText(text_position, text)
+
+            # Attempt to reload the image.
+            if self.file_path is not None:
+                self._load_image()
