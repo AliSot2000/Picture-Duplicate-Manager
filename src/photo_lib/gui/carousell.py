cc32f6f5019f615f178ccf431fb05b01b30bccd3
AliSot2000 <alisot200@gmail.com>
2023-08-13 01:42:37 +0200

    + Carousel is done.

--- src/photo_lib/gui/carousell.py
+++ src/photo_lib/gui/carousell.py
@@ -1,13 +1,14 @@
-from PyQt6.QtWidgets import QWidget, QApplication, QScrollArea, QHBoxLayout, QFrame
+from PyQt6.QtWidgets import QWidget, QApplication, QScrollArea, QHBoxLayout, QFrame, QLabel
 from PyQt6.QtGui import QPixmap, QPainter, QFont, QEnterEvent, QMouseEvent, QResizeEvent
 from PyQt6.QtCore import Qt, QRect, QPoint, QSize, pyqtSignal, QEvent, pyqtSlot, QSize, QPointF, QTimer
 import sys
 import os
-from typing import Union, List
+from typing import Union, List, Callable
 import math
 import warnings
-from image_tile import ImageTile
-from photo_lib.gui.clickable_image import ClickableImage
+from photo_lib.PhotoDatabase import BaseTileInfo
+from photo_lib.gui.clickable_image import ClickableTile
+from photo_lib.gui.model import Model
 
 """
 Information:
@@ -17,6 +18,17 @@ This is the first implementation of a basic carousel widget. Features that are m
 should be replaced by a single widget to free even more space.
 """
 
+def image_wrapper(image: ClickableTile, fn: Callable):
+    """
+    Given a function and a tile, bake tile as argument into function call for signal.
+    :param image: tile to bake in
+    :param fn: function to call
+    :return:
+    """
+    def wrapper():
+        fn(image=image)
+    return wrapper
+
 class Carousel(QScrollArea):
     """
     Carousel widget that displays a list of images and allows the user to scroll through them.
@@ -24,11 +36,17 @@ class Carousel(QScrollArea):
 
     child_dummy: QWidget
     h_layout: QHBoxLayout
-    images: List[ClickableImage]
-    current_select: ClickableImage = None
+    images: List[ClickableTile]
+    current_select: ClickableTile = None
+    image_changed = pyqtSignal(BaseTileInfo)
 
-    def __init__(self):
+    model = Model
+
+    def __init__(self, model: Model):
         super().__init__()
+
+        self.model = model
+        self.images = []
 
         self.setWidgetResizable(True)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -39,7 +57,38 @@ class Carousel(QScrollArea):
         self.h_layout = QHBoxLayout()
         self.child_dummy.setLayout(self.h_layout)
 
-        self.images: List[ClickableImage] = []
+    def build_carousel(self, target: BaseTileInfo = None):
+        """
+        Builds the carousel and sets the target image if provided. Otherwise, no image is set.
+        :param target:
+        :return:
+        """
+        # Fetch all tiles
+        tiles = self.model.tile_infos
+        target_i = None
+
+        for i in range(len(tiles)):
+        # for i in range(2):
+            # Get info
+            tile = tiles[i]
+
+            # Create the Widget
+            img = ClickableTile()
+            img.setFixedHeight(100)
+            img.setMinimumWidth(self.height())
+            img.tile_info = tile
+
+            # When clicked, set the image to the clicked image
+            img.clicked.connect(image_wrapper(img, self.set_image))
+            self.images.append(img)
+            self.h_layout.addWidget(img)
+
+            # Check for target, if the keys match keep track of the image
+            if target is not None and target.key == tile.key:
+                target_i = i
+
+        if target_i is not None:
+            self.set_image(self.images[target_i])
 
     def resizeEvent(self, a0: QResizeEvent) -> None:
         """
@@ -48,9 +97,12 @@ class Carousel(QScrollArea):
         :return:
         """
         super().resizeEvent(a0)
-        self.child_dummy.setMaximumHeight(a0.size().height())
+        self.child_dummy.setMaximumHeight(self.size().height())
+        for img in self.images:
+            img.setMinimumWidth(self.height())
+            img.setMaximumHeight(self.height())
 
-    def set_image(self, image: ClickableImage):
+    def set_image(self, image: ClickableTile):
         """
         Set the image to be displayed and add a Frame around it.
         :param image:
@@ -58,8 +110,9 @@ class Carousel(QScrollArea):
         """
         self.ensureWidgetVisible(image)
         self.mark_image(image)
+        self.image_changed.emit(image.tile_info)
 
-    def mark_image(self, image: ClickableImage):
+    def mark_image(self, image: ClickableTile):
         """
         Mark the image as selected.
         :param image:
@@ -76,11 +129,27 @@ class Carousel(QScrollArea):
         Adds a frame around the current_selected image
         :return:
         """
-        self.current_select.setFrameStyle(QFrame.Shape.Box)
+        self.current_select.setStyleSheet("padding: 10px; "
+                                           "border: 2px solid black;")
 
     def unmark_selected(self):
         """
         Removes the frame around the current_selected image
         :return:
         """
-        self.current_select.setFrameStyle(QFrame.Shape.NoFrame)
+        self.current_select.setStyleSheet("padding: 10px; ")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = Carousel(Model(folder_path="/media/alisot2000/DumpStuff/dummy_db/"))
+    window.model.current_import_table_name = "tbl_1998737548188488947"
+    window.model.build_tiles_from_table()
+    window.setWindowTitle("Carousel Test")
+    window.build_carousel()
+
+    window.image_changed.connect(lambda x: print(x))
+
+    window.show()
+    sys.exit(app.exec())
