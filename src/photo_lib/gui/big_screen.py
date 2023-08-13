04532a870a3694dbd6c6fd19cb863f187e23a800
AliSot2000 <alisot200@gmail.com>
2023-08-13 18:31:16 +0200

    + Work in progress.

--- src/photo_lib/gui/big_screen.py
+++ src/photo_lib/gui/big_screen.py
@@ -1,4 +1,16 @@
-from PyQt6.QtWidgets import QFrame
+from PyQt6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea
+from photo_lib.gui.model import Model
+from photo_lib.gui.carousell import Carousel
+from photo_lib.gui.zoom_image import ZoomImage
+from photo_lib.PhotoDatabase import BaseTileInfo, FullImportTableEntry
 
 
-class BigScreen(QFrame)
+class BigScreen(QFrame):
+    model: Model = None
+    source_tile: BaseTileInfo = None
+    full_info: FullImportTableEntry = None
+
+    h_layout: QHBoxLayout
+    main_image: ZoomImage
+
+    metadata = QScrollArea
