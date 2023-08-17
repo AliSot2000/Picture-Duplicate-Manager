2334021dc7fa0da0a5a9a3913c79c53b57f42c27
AliSot2000 <alisot200@gmail.com>
2023-08-17 15:12:34 +0200

    + First iteration of the big_screen view.

--- src/photo_lib/gui/big_screen.py
+++ src/photo_lib/gui/big_screen.py
@@ -1,16 +1,60 @@
-from PyQt6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea
+from PyQt6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea, QSplitter, QApplication
+from PyQt6.QtCore import Qt
 from photo_lib.gui.model import Model
 from photo_lib.gui.carousell import Carousel
-from photo_lib.gui.zoom_image import ZoomImage
-from photo_lib.PhotoDatabase import BaseTileInfo, FullImportTableEntry
+from photo_lib.gui.image_viewer import ImportImageView
+import sys
 
 
-class BigScreen(QFrame):
+
+class BigScreen(QSplitter):
     model: Model = None
-    source_tile: BaseTileInfo = None
-    full_info: FullImportTableEntry = None
 
-    h_layout: QHBoxLayout
-    main_image: ZoomImage
+    carousel: Carousel = None
+    image_viewer: ImportImageView = None
 
-    metadata = QScrollArea
+    def __init__(self, model: Model):
+        super().__init__(Qt.Orientation.Vertical)
+        self.model = model
+
+        self.carousel = Carousel(model=model)
+        self.carousel.build_carousel()
+        self.image_viewer = ImportImageView(model=model)
+
+        self.addWidget(self.image_viewer)
+        self.addWidget(self.carousel)
+
+        self.setStretchFactor(0, 1)
+        self.setStretchFactor(1, 0)
+
+        self.carousel.image_changed.connect(self.update_image_view)
+
+    def update_image_view(self):
+        """
+        Doing it like this because None somehow didn't work correctly.
+        :return:
+        """
+        self.image_viewer.tile_info = self.carousel.current_select.tile_info
+
+    # TODO actions next and previous image
+    def build_all(self):
+        """
+        Builds all the child widgets from the current model.
+        :return:
+        """
+        self.carousel.build_carousel()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = BigScreen(Model(folder_path="/media/alisot2000/DumpStuff/dummy_db/"))
+    window.model.current_import_table_name = "tbl_1998737548188488947"
+    window.model.build_tiles_from_table()
+    window.build_all()
+
+    window.setWindowTitle("Big Screen")
+
+    window.show()
+    sys.exit(app.exec())
