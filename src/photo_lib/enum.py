2ca02212b3d4d333c82209c13a8c9c4536fc8d3a
AliSot2000 <alisot200@gmail.com>
2023-08-17 15:11:23 +0200

    + Created new file with all enums from the Database.

---
+++ src/photo_lib/enum.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class GUICommandTypes(Enum):
+    NONE = 0
+    QUIT = 1
+
+class ProcessComType(Enum):
+    MAX = 1
+    CURRENT = 2
+    MESSAGE = 3
+
+
+class MatchTypes(Enum):
+    """
+    Enum to indicate the type of match found in the database.
+    """
+    No_Match = 0
+    Binary_Match_Images = 1
+    Binary_Match_Trash = 2
+    Hash_Match_Trash = 3
+    Binary_Match_Replaced = 4
+    Hash_Match_Replaced = 5
+
+class GoogleFotosMetadataStatus(Enum):
+    No_Metadata = -1
+    Copied_Metadata = 0
+    Original_Metadata = 1
