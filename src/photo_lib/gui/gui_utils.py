29365bcab86999c2d505c10039205f9c9592c1c6
AliSot2000 <alisot200@gmail.com>
2023-08-22 00:55:07 +0200

    + Added more functions + Added stub class because of circular import

--- src/photo_lib/gui/gui_utils.py
+++ src/photo_lib/gui/gui_utils.py
@@ -1,10 +1,20 @@
+from PyQt6.QtWidgets import QPushButton
 from photo_lib.gui.clickable_image import ClickableTile
 from typing import Union, List, Callable
+
+
+class MediaPane:
+    pass
 
 
 """
 Utility functions - namely wrapper functions to bake an argument into a function call for a signal.
 """
+def bake_attribute(name: str, func: Callable):
+    def baked_attribute_func(*args, **kwargs):
+        return func(*args, name=name,**kwargs)
+
+    return baked_attribute_func
 
 
 def image_wrapper(image: ClickableTile, fn: Callable):
@@ -16,4 +26,45 @@ def image_wrapper(image: ClickableTile, fn: Callable):
     """
     def wrapper():
         fn(image=image)
+    return wrapper
+
+
+def button_wrapper(btn: QPushButton, func: Callable):
+    """
+    This function is used to wrap the button that calls the function into the function so that one function can be
+    used for many different buttons. Because Slots in QT don't communicate the caller of the function.
+
+    :param btn: button that calls the function
+    :param func: methode to execute that needs to have the button as a parameter
+    :return:
+    """
+    def wrapper():
+        return func(btn=btn)
+    return wrapper
+
+
+def pain_wrapper(media_pane: MediaPane, func: Callable):
+    """
+    This function is used to wrap the media pain that calls the function into the function so that one function can be
+    used for many different buttons. Because Slots in QT don't communicate the caller of the function.
+
+    :param media_pane: pain that calls the function
+    :param func: methode to execute that needs to have the button as a parameter
+    :return:
+    """
+    def wrapper():
+        return func(media_pane=media_pane)
+    return wrapper
+
+
+def path_wrapper(path: str, func: Callable):
+    """
+    Given a path at time of creation, bakes the path into the function call since the function called in signals may not
+    have any arguments.
+    :param path: Path to bake in
+    :param func: function to call with it
+    :return:
+    """
+    def wrapper():
+        return func(path=path)
     return wrapper
