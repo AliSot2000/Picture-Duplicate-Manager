d98fb14c3fd7e476a0005146049e0a67a1ff5d8c
AliSot2000 <alisot200@gmail.com>
2023-08-24 18:41:12 +0200

    + Initializing with table to test.

--- src/photo_lib/run_gui.py
+++ src/photo_lib/run_gui.py
@@ -1,9 +1,17 @@
 from photo_lib.gui.main_window import RootWindow
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QImageReader
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = RootWindow()
+    window = RootWindow(external_setup=True)
+    window.model.set_folder_path("/media/alisot2000/DumpStuff/work_dummy/")
+    window.model.current_import_table_name = "tbl_-2130229211680430806"
+    window.model.import_folder = "/media/alisot2000/DumpStuff/Panos"
+    window.build_import_views()
+    # window.open_import_big_screen()
+    h = QImageReader.setAllocationLimit(0)
     window.show()
     sys.exit(app.exec())
