4672d101a5b34dc0565341f72d13dfd36648c03d
AliSot2000 <alisot200@gmail.com>
2023-08-24 19:53:04 +0200

    + Testing and shit.

--- src/photo_lib/run_gui.py
+++ src/photo_lib/run_gui.py
@@ -6,11 +6,12 @@ from PyQt6.QtGui import QImageReader
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = RootWindow(external_setup=True)
-    window.model.set_folder_path("/media/alisot2000/DumpStuff/work_dummy/")
-    window.model.current_import_table_name = "tbl_-2130229211680430806"
-    window.model.import_folder = "/media/alisot2000/DumpStuff/Panos"
-    window.build_import_views()
+    window = RootWindow()
+    # window = RootWindow(external_setup=True)
+    # window.model.set_folder_path("/media/alisot2000/DumpStuff/work_dummy/")
+    # window.model.current_import_table_name = "tbl_-2130229211680430806"
+    # window.model.import_folder = "/media/alisot2000/DumpStuff/Panos"
+    # window.build_import_views()
     # window.open_import_big_screen()
     h = QImageReader.setAllocationLimit(0)
     window.show()
