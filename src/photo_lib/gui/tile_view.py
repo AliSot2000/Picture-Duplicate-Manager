2be872a99420af842a2eadce1e0cf7ce4e222d75
AliSot2000 <alisot200@gmail.com>
2024-10-22 15:07:57 +0200

    + Different Printing

--- src/photo_lib/gui/tile_view.py
+++ src/photo_lib/gui/tile_view.py
@@ -424,7 +424,10 @@ class TileWidget(QFrame):
         Given the maximum number of widgets displayed at time, increase the number of widgets if resize requires it.
         """
         # Guard if it's less or equal
-        print(f"Number of generated rows: {self.number_of_generated_rows}, Number of columns: {self.number_of_columns}")
+        print(f"Number of generated rows: {self.number_of_generated_rows}, "
+              f"Number of columns: {self.number_of_columns}, "
+              f"Number of Widgets: {self.number_of_generated_rows * self.number_of_columns}")
+
         if self.number_of_generated_rows * self.number_of_columns <= len(self.widgets):
             return
 
