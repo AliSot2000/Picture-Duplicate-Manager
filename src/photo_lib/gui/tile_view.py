2050949ab78f4b283d198749c15cb8d7aa904a3f
AliSot2000 <alisot200@gmail.com>
2024-10-22 14:58:55 +0200

    + Formatting

--- src/photo_lib/gui/tile_view.py
+++ src/photo_lib/gui/tile_view.py
@@ -75,8 +75,10 @@ class TileWidget(QFrame):
     # Widgets and Layout structures
     widgets: List[IndexedTile] = None  # List of all widgets that are currently instantiated
     hidden_widgets: List[IndexedTile] = None  # Widgets that are currently hidden
+
     widget_rows: List[List[IndexedTile]] = None
     layout_rows: List[Union[QLabel, List[IndexedTile]]] = None
+
     background_widget: QWidget = None
     background_layout: QGridLayout = None
 
