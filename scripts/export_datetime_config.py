6ab396115c31f5737ee8c5f812b4bedfb5079193
AliSot2000 <alisot200@gmail.com>
2025-01-08 17:27:52 +0100

    + Generate Datetime

---
+++ scripts/export_datetime_config.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+
+human_file = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "src", "photo_lib", "datetime_human_fmt.json"))
+
+comp_file = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "src", "photo_lib", "datetime_fmt.json"))
+
+hstat = os.stat(human_file)
+cstat = os.stat(comp_file)
+
+if hstat.st_mtime > cstat.st_mtime:
+    print(f"{human_file} is newer than {comp_file} writing new compressed file")
+    with open(human_file, "r") as f:
+        data = json.load(f)
+
+    with open(comp_file, "w") as f:
+        json.dump(data, f)
