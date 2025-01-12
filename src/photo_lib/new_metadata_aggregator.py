9854f66282f6727687c9ed18aeb0cec7c91e646b
AliSot2000 <alisot200@gmail.com>
2025-01-12 23:42:37 +0100

    + Bugfix, == needed in assdert

--- src/photo_lib/new_metadata_aggregator.py
+++ src/photo_lib/new_metadata_aggregator.py
@@ -385,7 +385,7 @@ class NewMetadataAggregator:
                                 DateTimeSource.GOOGLE_PHOTOS_UNAWARE,
                                 DateTimeSource.DATE_OR_TIME,
                                 DateTimeSource.FILE_AWARE]
-            assert len(self.tz_priority) < len(DateTimeSource._member_names_), "Not all Sources covered. Fix CLass"
+            assert len(self.tz_priority) == len(DateTimeSource._member_names_), "Not all Sources covered. Fix CLass"
 
         if ignore_keys is not None:
             self.ignore_keys = ignore_keys
