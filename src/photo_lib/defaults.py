cf4c85cd535c9dcab05a7b5b2a36d8a855a0b188
AliSot2000 <alisot200@gmail.com>
2025-01-20 21:20:22 +0100

    + Removed default from all variable names

--- src/photo_lib/defaults.py
+++ src/photo_lib/defaults.py
@@ -4,10 +4,10 @@ import os
 File contains functions needed to initialize a database. To make future code changes easier, the defaults are inside
 this file as opposed to the photo_db file.
 """
-default_image_extensions = [".jpeg", ".jpg", ".png", '.3gp', '.dng', '.heic', '.heif', '.webp', '.tif', '.tiff']
-default_video_extensions = ['.mp4', '.m4v', '.mov', '.gif']
+image_extensions = [".jpeg", ".jpg", ".png", '.3gp', '.dng', '.heic', '.heif', '.webp', '.tif', '.tiff']
+video_extensions = ['.mp4', '.m4v', '.mov', '.gif']
 
-default_extensions = set(default_image_extensions + default_video_extensions)
+extensions = set(image_extensions + video_extensions)
 
 dipy_union_photo_db = {'.sun', '.dds', '.mp4', '.dcx', '.jpf', '.dib', '.webp', '.spider', '.iim', '.j2k', '.pcd',
                       '.3gp', '.gd2', '.heif', '.psd', '.dng', '.ppm', '.sgi', '.mpo', '.tga', '.xbm', '.msp', '.fli',
@@ -17,8 +17,10 @@ dipy_union_photo_db = {'.sun', '.dds', '.mp4', '.dcx', '.jpf', '.dib', '.webp', 
                       '.xpm', '.bw', '.cur', '.jpg', '.jpeg', '.im', '.wal', '.ras', '.rgba', '.pgm', '.emf', '.ico',
                       '.rgb'}
 
-default_trash_path = lambda root: os.path.join(root, '.trash')
-default_thumbnails_path = lambda root: os.path.join(root, '.thumbnails')
-default_temp_path = lambda root: os.path.join(root, '.temp')
-default_config_path = lambda root: os.path.join(root, '.config.json')
-default_db_file = ".photos.db"
+trash_path = lambda root: os.path.join(root, '.trash')
+thumbnails_path = lambda root: os.path.join(root, '.thumbnails')
+temp_path = lambda root: os.path.join(root, '.temp')
+config_path = lambda root: os.path.join(root, '.config.json')
+db_file = ".photos.db"
+miniature_size = 512
+thumbnail_size = 64
