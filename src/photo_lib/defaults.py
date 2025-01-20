86cc13f5980ef43d6abffde1873546753bca1db8
AliSot2000 <alisot200@gmail.com>
2025-01-20 19:26:38 +0100

    + Added default config + Added extensions from difpy + Added extensions from this project

--- src/photo_lib/defaults.py
+++ src/photo_lib/defaults.py
@@ -4,8 +4,12 @@ import os
 File contains functions needed to initialize a database. To make future code changes easier, the defaults are inside
 this file as opposed to the photo_db file.
 """
+default_image_extensions = [".jpeg", ".jpg", ".png", '.3gp', '.dng', '.heic', '.heif', '.webp', '.tif', '.tiff']
+default_video_extensions = ['.mp4', '.m4v', '.mov', '.gif']
 
-default_extensions = {'.sun', '.dds', '.mp4', '.dcx', '.jpf', '.dib', '.webp', '.spider', '.iim', '.j2k', '.pcd',
+default_extensions = set(default_image_extensions + default_video_extensions)
+
+dipy_union_photo_db = {'.sun', '.dds', '.mp4', '.dcx', '.jpf', '.dib', '.webp', '.spider', '.iim', '.j2k', '.pcd',
                       '.3gp', '.gd2', '.heif', '.psd', '.dng', '.ppm', '.sgi', '.mpo', '.tga', '.xbm', '.msp', '.fli',
                       '.tif', '.jif', '.gd', '.m4v', '.gbr', '.imt', '.flc', '.eps', '.icb', '.jpe', '.jpm', '.fpx',
                       '.fits', '.apng', '.pbm', '.mov', '.ftex', '.png', '.icns', '.jfif', '.nc', '.heic', '.jfi',
@@ -16,4 +20,5 @@ default_extensions = {'.sun', '.dds', '.mp4', '.dcx', '.jpf', '.dib', '.webp', '
 default_trash_path = lambda root: os.path.join(root, '.trash')
 default_thumbnails_path = lambda root: os.path.join(root, '.thumbnails')
 default_temp_path = lambda root: os.path.join(root, '.temp')
+default_config_path = lambda root: os.path.join(root, '.config.json')
 default_db_file = ".photos.db"
