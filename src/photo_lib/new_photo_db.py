bfc42708bdd997cfc936d04c3e656c01f8ffd5d9
AliSot2000 <alisot200@gmail.com>
2025-01-25 01:02:49 +0100

    + Added TODO

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -104,6 +104,7 @@ class PhotoDB(BaseSQliteDB):
         """
         Create a new config with only defaults.
         """
+        # TODO paths should be relative
         return Config(
             version=current_version.current_version,
             image_extensions=defaults.image_extensions,
