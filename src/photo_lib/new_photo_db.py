a4619067fe0ed26d74b5e3edaf37b941d85f48e5
AliSot2000 <alisot200@gmail.com>
2025-02-02 15:50:22 +0100

    + Added return of new table name (in case it's generic) + Added new function to check if a given import table exists

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -215,6 +215,14 @@ class PhotoDB(BaseSQliteDB):
         decl = self.generic_decls["import_table"]
         self.debug_execute(decl.declaration_string.replace(decl.name_placeholder, tbl_name))
         self.commit()
+        return tbl_name
+
+    def import_table_exists(self, name: str = None) -> bool:
+        """
+        Check if a given name with root_path and name exists already.
+        """
+        self.debug_execute("SELECT key FROM import_tables WHERE  name = ?", ( name))
+        return self.sq_cur.fetchone() is not None
 
     def remove_import_table(self, name: str = None) -> Tuple[bool, bool]:
         """
