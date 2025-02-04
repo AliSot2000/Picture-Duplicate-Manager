9bcefbe93c684d312121060ed28f66051ac5c297
AliSot2000 <alisot200@gmail.com>
2025-02-04 18:37:09 +0100

    + Added insert_get_gps_loc to insert and get key of gps row

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -1113,6 +1113,23 @@ class PhotoDB(BaseSQliteDB):
 
         return list(keys.keys()), highest_match_key, highest_match
 
+    def insert_get_gps_loc(self, gps_lat: float, gps_long: float):
+        """
+        Get the Key of a given pair of GPS coordinates. If it doesn't exist, add it to the GPS Table.
+        """
+        self.debug_execute("SELECT key FROM gps_location WHERE gps_latitude = ? AND gps_longitude = ?",
+                           (gps_lat, gps_long))
+
+        res = self.sq_cur.fetchone()
+        if res is not None:
+            return res[0]
+
+        # PRECONDITION: GPS Location doesn't exist
+        self.debug_execute("INSERT INTO gps_location (gps_latitude, gps_longitude) VALUES (?, ?)",
+                           (gps_lat, gps_long))
+        key = self.insert_get_gps_loc(gps_lat, gps_long)
+        return key
+
 
     # ==================================================================================================================
     # Deduplication
