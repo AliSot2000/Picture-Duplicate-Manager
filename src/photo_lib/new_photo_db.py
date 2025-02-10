024e9be7cfe6e687a887586a500879674afab12a
AliSot2000 <alisot200@gmail.com>
2025-02-10 23:50:48 +0100

    + Updated Imports

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -15,7 +15,7 @@ import ffmpeg
 import photo_lib.defaults as defaults
 from photo_lib.cache import Cache, nd
 from photo_lib.config import Config
-from photo_lib.custom_enum import GroupingCriterion, DBLocation, NewMatchTypes
+from photo_lib.custom_enum import GroupingCriterion, NewMatchTypes
 from photo_lib.db_definitions import current_version, history, StaticDeclaration, GenericDeclaration
 from photo_lib.errors_and_warnings import ImplementationError, CorruptDatabase
 from photo_lib.flag_dataclasses import MainFlags
