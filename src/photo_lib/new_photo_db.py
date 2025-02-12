cee34b51f65dc18275e3ee40d1080b54763d8b87
AliSot2000 <alisot200@gmail.com>
2025-02-12 15:36:48 +0100

    + Added function to create all necessary directories

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -201,6 +201,23 @@ class PhotoDB(BaseSQliteDB):
             datetime_fmt=self.config.datetime_fmt
         )
 
+    def check_create_default_dirs(self):
+        """
+        Create default directories needed for the db.
+        """
+        if not os.path.exists(self.get_trash_dir()):
+            self.main_logger.info(f"Created Trash Directory")
+            os.makedirs(self.get_trash_dir())
+
+        if not os.path.exists(self.get_thumb_dir()):
+            self.main_logger.info(f"Created Thumbnail Directory")
+            os.makedirs(self.get_thumb_dir())
+
+        if not os.path.exists(self.get_temp_dir()):
+            self.main_logger.info(f"Created Temp Directory")
+            os.makedirs(self.get_temp_dir())
+
+
     # ==================================================================================================================
     # Table Creation & Deletion & Modify Functions
     # ==================================================================================================================
