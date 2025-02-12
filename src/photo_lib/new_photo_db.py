d815d6a56dc3cd8655cecbc11a33f0ced18fd386
AliSot2000 <alisot200@gmail.com>
2025-02-12 15:41:31 +0100

    + Added change_parent + Implemented change_parent + Added list_children + Implemented list_children

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -2459,6 +2459,19 @@ class PhotoDB(BaseSQliteDB):
         """
         # TODO implement
 
+    def change_parent(self, key: int, new_parent: int):
+        """
+        Option to change a parent of a duplicate file, needed to undo an erroneous selection of the parent
+        """
+        self.debug_execute("UPDATE main SET parent = ? WHERE key = ?", (new_parent, key))
+
+    def list_children(self, key: int) -> List[int]:
+        """
+        List all files which have the given key as parent.
+        """
+        self.debug_execute("SELECT key FROM main WHERE parent = ?", (key,))
+        return [res[0] for res in self.sq_cur.fetchall()]
+
     def delete_trash_thumb(self, key: Union[List[int], int, None]) -> int:
         """
         Delete the remaining thumbnail of an image in the trash. For recognition purposes, the thumbnails of the
