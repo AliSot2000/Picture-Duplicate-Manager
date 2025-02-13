de90f6c63adec03988895f8387d01a6ef460b647
AliSot2000 <alisot200@gmail.com>
2025-02-13 22:17:46 +0100

    + Moved get_newest_hash

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -793,6 +793,24 @@ class PhotoDB(BaseSQliteDB):
         self.debug_execute("SELECT COUNT(main_key) FROM hash_update_table")
         return self.sq_cur.fetchone()[0]
 
+    def get_newest_hash(self, key: int) -> Tuple[str, int] | Tuple[None, None]:
+        """
+        Get the newest hash of a given file. If the newest hash doesn't match, we don't perform binary comparison.
+
+        :param key: File key to search for
+        :returns: Tuple[None, None] -> key not found, Tuple[str, int] -> newest hash and file_size_bytes of that hash.
+        """
+        self.debug_execute("SELECT h.hash, ha.file_size_bytes "
+                           "FROM hashes AS h JOIN hash_assoz AS ha ON h.key = ha.hash_key "
+                           "WHERE ha.file_key = ? AND ha.hash_date IN "
+                           "(SELECT MAX(hash_date) FROM hash_assoz WHERE file_key = ?)",
+                           (key, key))
+        res = self.sq_cur.fetchone()
+        if res is None:
+            return None, None
+
+        return res[0], res[1]
+
     # TODO move
     def update_hash_from_filename_table(self) -> Tuple[int, int]:
         """
@@ -2156,24 +2174,6 @@ class PhotoDB(BaseSQliteDB):
                            (db_name, key))
         self.debug_execute("UPDATE metadata SET db_dir = ? WHERE main_key = ?",
                            (dir_key, key))
-
-    def get_newest_hash(self, key: int) -> Tuple[str, int] | Tuple[None, None]:
-        """
-        Get the newest hash of a given file. If the newest hash doesn't match, we don't perform binary comparison.
-
-        :param key: File key to search for
-        :returns: Tuple[None, None] -> key not found, Tuple[str, int] -> newest hash and file_size_bytes of that hash.
-        """
-        self.debug_execute("SELECT h.hash, ha.file_size_bytes "
-                           "FROM hashes AS h JOIN hash_assoz AS ha ON h.key = ha.hash_key "
-                           "WHERE ha.file_key = ? AND ha.hash_date IN "
-                           "(SELECT MAX(hash_date) FROM hash_assoz WHERE file_key = ?)",
-                           (key, key))
-        res = self.sq_cur.fetchone()
-        if res is None:
-            return None, None
-
-        return res[0], res[1]
 
     def _get_best_match_type(self, tgt_fp: str, file_hash: str, fsb: int) \
             -> Tuple[Dict[int, NewMatchTypes], int | None, NewMatchTypes]:
