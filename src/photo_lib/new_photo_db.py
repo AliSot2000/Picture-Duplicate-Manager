bf70fbf23c8155953859f45795b80f1291473560
AliSot2000 <alisot200@gmail.com>
2025-02-13 18:07:40 +0100

    + Implemented update_hash_from_filename

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -541,13 +541,33 @@ class PhotoDB(BaseSQliteDB):
     # DB Integrity checks and utility
     # ==================================================================================================================
 
-    def update_hash_from_filename(self, fname: Dict[str, str]):
+    def update_hash_from_filename(self) -> Tuple[int, int]:
         """
         Updates the hash of the image file with the given file name.
-        """
-        ...
 
-    def update_filename_from_hash(self, new_names: Dict[int, str]):
+        :return: number of new entries in hash_assoz table, number of hashes updated
+        """
+        if self.hash_update_table_empty():
+            raise ValueError("Hash table is empty")
+
+        # Get the size of the hash assoz table
+        current_size = self.get_size_of_hash_assoz_table()
+
+        self.add_extra_cursor("update_hash")
+        self.debug_execute(stmt="SELECT main_key, new_hash, file_size_bytes FROM hash_update_table",
+                           cur="update_hash")
+
+        count = 0
+        for mk, nh, fsb in self.get_cursor("update_hash"):
+            self.check_add_file_hash(file_hash=nh, file_key=mk, file_size=fsb)
+            count += 1
+
+        new_size = self.get_size_of_hash_assoz_table()
+
+        self.main_logger.info(f"Updated {count} file hashes. {new_size - current_size} of unseen hashes.")
+        return new_size - current_size, count
+
+    def update_filename_from_hash(self, move: bool):
         """
         Update the names of files resolved through hash and filesize.
         """
