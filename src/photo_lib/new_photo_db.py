edf575bcc5d0b80ac5ca888392dec7acfac6ba4d
AliSot2000 <alisot200@gmail.com>
2025-02-13 18:14:02 +0100

    + changed type of matches in import table

--- src/photo_lib/new_photo_db.py
+++ src/photo_lib/new_photo_db.py
@@ -1756,14 +1756,13 @@ class PhotoDB(BaseSQliteDB):
             target_fp = str(os.path.join(original_dirname, original_filename))
             assert os.path.exists(target_fp), "Import file needs to exist."
 
-            keys, highest_key, highest_match = self._get_best_match_type(tgt_fp=target_fp,
+            matches, highest_key, highest_match = self._get_best_match_type(tgt_fp=target_fp,
                                                                          file_hash=file_hash,
                                                                          fsb=file_size_bytes)
 
             self.debug_execute(stmt=f"UPDATE `{tbl_name}` SET match_type = ?, highest_match = ?, matches = ? "
                                     f"WHERE key = {key}",
-                               args=(highest_match.value, highest_match,
-                                     json.dumps(keys).replace("'", "''"), key))
+                               args=(highest_match.value, highest_match, json.dumps(matches), key))
             count += 1
 
         self.main_logger.info(f"Found {count} matches for {tbl_name}")
