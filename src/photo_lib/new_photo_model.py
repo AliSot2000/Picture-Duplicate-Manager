2372f4f6d058897f1913fea722b0be6df2521bd1
AliSot2000 <alisot200@gmail.com>
2025-02-26 22:14:47 +0100

    + Updated Imports

--- src/photo_lib/new_photo_model.py
+++ src/photo_lib/new_photo_model.py
@@ -14,7 +14,7 @@ import ffmpeg
 import photo_lib.defaults as defaults
 from photo_lib.cache import Cache, nd
 from photo_lib.config import Config
-from photo_lib.custom_enum import GroupingCriterion, NewMatchTypes, SelectionType, MediaType, Allowed
+from photo_lib.custom_enum import GroupingCriterion, NewMatchTypes, SelectionType, MediaType, Allowed, ImportStatus
 from photo_lib.data_objects import Selection
 from photo_lib.new_photo_db import PhotoDB
 from photo_lib.db_definitions import current_version, history, StaticDeclaration, GenericDeclaration
