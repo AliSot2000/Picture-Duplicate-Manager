17c30652109daf13ccda1db623be78b4a27a0b92
AliSot2000 <alisot200@gmail.com>
2025-03-06 20:50:31 +0100

    + Test file for testing setting the exiftag

---
+++ scratch/set_exif_tag.py
@@ -0,0 +1,27 @@
+import exiftool
+
+
+def set_create_date(file_path, create_date):
+    """
+    Set the EXIF:CreateDate tag for an image file using pyexiftool.
+
+    Parameters:
+    - file_path: Path to the image file.
+    - create_date: Date in the format 'YYYY:MM:DD HH:MM:SS'.
+    """
+    with exiftool.ExifTool() as et:
+        # Prepare the EXIF tag with the correct format
+        metadata = {f"EXIF:CreateDate": create_date}
+
+        # Update the metadata
+        et.set_tags(metadata, [file_path])
+        print(f"Successfully set EXIF:CreateDate to {create_date} for file {file_path}.")
+
+
+# Example usage:
+image_path = "example.jpg"
+new_create_date = "2024:12:30 12:00:00"
+set_create_date(image_path, new_create_date)
+
+# Need to set   OffsetTime = +/-
+# Need to set   ModifyDate = YYYY:MM:DDTHH:MM:SS
