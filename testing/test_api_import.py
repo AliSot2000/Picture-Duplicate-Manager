52cb9c63dd8a8e9f028692e1a081e9e636179ee1
AliSot2000 <alisot200@gmail.com>
2025-03-07 14:35:51 +0100

    + Switched to API attr

--- testing/test_api_import.py
+++ testing/test_api_import.py
@@ -92,85 +92,74 @@ class TestAPIPrepareDirectoryForImport(unittest.TestCase):
         child_of_root = os.path.join(self.temp_db, "some_dir")
         os.makedirs(child_of_root, exist_ok=True)
 
-        db = PhotoAPI(root_path=self.temp_db)
-
-        child_of_temp = os.path.join(db.db.get_temp_dir(), "some_dir")
-        child_of_thumb = os.path.join(db.db.get_thumb_dir(), "some_dir")
-        child_of_trash = os.path.join(db.db.get_trash_dir(), "some_dir")
+        child_of_temp = os.path.join(self.api.db.get_temp_dir(), "some_dir")
+        child_of_thumb = os.path.join(self.api.db.get_thumb_dir(), "some_dir")
+        child_of_trash = os.path.join(self.api.db.get_trash_dir(), "some_dir")
 
         os.makedirs(child_of_temp, exist_ok=True)
         os.makedirs(child_of_thumb, exist_ok=True)
         os.makedirs(child_of_trash, exist_ok=True)
 
         # Check the default problems
-        self.assertRaises(TypeError, lambda: db.prepare_directory_for_import(source_dir=rel_root))
-        self.assertRaises(TypeError, lambda: db.prepare_directory_for_import(source_dir=root_file))
-        self.assertRaises(ValueError, lambda: db.prepare_directory_for_import(source_dir=child_of_root))
+        self.assertRaises(TypeError, lambda: self.api.prepare_directory_for_import(source_dir=rel_root))
+        self.assertRaises(TypeError, lambda: self.api.prepare_directory_for_import(source_dir=root_file))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=child_of_root))
 
         # Check the db directories
-        self.assertRaises(ValueError, lambda: db.prepare_directory_for_import(source_dir=child_of_temp))
-        self.assertRaises(ValueError, lambda: db.prepare_directory_for_import(source_dir=child_of_thumb))
-        self.assertRaises(ValueError, lambda: db.prepare_directory_for_import(source_dir=child_of_trash))
-
-        db.cleanup(fast=True)
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=child_of_temp))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=child_of_thumb))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=child_of_trash))
 
     def test_append_purge_errors(self):
         """
         Test alternate possibilities with append and purge set
         """
-        db = PhotoAPI(root_path=self.temp_db)
         db_dir = os.path.join(self.media_source, "db")
 
         # Both not allowed
-        self.assertRaises(ValueError, lambda : db.prepare_directory_for_import(source_dir=db_dir,
-                                                                               append=True,
-                                                                               purge=True))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                                                    append=True,
+                                                                                    purge=True))
 
         # Table empty
-        self.assertRaises(ValueError, lambda : db.prepare_directory_for_import(source_dir=db_dir,
-                                                                               append=True,
-                                                                               purge=False))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                                                    append=True,
+                                                                                    purge=False))
 
         # Table empty
-        self.assertRaises(ValueError, lambda : db.prepare_directory_for_import(source_dir=db_dir,
-                                                                               append=False,
-                                                                               purge=True))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                                                    append=False,
+                                                                                    purge=True))
 
         # Test table exists
-        self.assertRaises(ValueError, lambda : db.prepare_directory_for_import(source_dir=db_dir,
-                                                                               append=True,
-                                                                               purge=False,
-                                                                               tbl_name="some_table"))
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                                                    append=True,
+                                                                                    purge=False,
+                                                                                    tbl_name="some_table"))
 
         # Actually perform import:
-        tbl = db.prepare_directory_for_import(source_dir=db_dir,
-                                              append=False,
-                                              purge=False)
+        tbl = self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                    append=False,
+                                                    purge=False)
 
         # Error, Table Exists
-        self.assertRaises(ValueError, lambda : db.prepare_directory_for_import(source_dir=db_dir,
-                                                                               tbl_name=tbl))
-
-
-        # Perform import
-        db.cleanup(fast=True)
+        self.assertRaises(ValueError, lambda: self.api.prepare_directory_for_import(source_dir=db_dir,
+                                                                                    tbl_name=tbl))
 
     def test_append(self):
         """
         Test that appending to import table works
         """
-        db = PhotoAPI(root_path=self.temp_db)
-
         shutil.copytree(os.path.join(self.media_source, "db"), self.import_source)
 
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source)
-        tbl_size = db.db.get_size_of_single_import_table(tbl)
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source)
+        tbl_size = self.api.db.get_size_of_single_import_table(tbl)
 
         self.assertEqual(tbl_size, 152)
 
         # Test empty append
-        db.prepare_directory_for_import(source_dir=self.import_source, append=True, tbl_name=tbl)
-        tbl_size_1 = db.db.get_size_of_single_import_table(tbl)
+        self.api.prepare_directory_for_import(source_dir=self.import_source, append=True, tbl_name=tbl)
+        tbl_size_1 = self.api.db.get_size_of_single_import_table(tbl)
 
         self.assertEqual(tbl_size_1, 152)
 
@@ -181,137 +170,116 @@ class TestAPIPrepareDirectoryForImport(unittest.TestCase):
         shutil.copytree(allowed_test, os.path.join(self.import_source, "import_base_dir"))
 
         # Prepare the next 4 files.
-        db.prepare_directory_for_import(source_dir=self.import_source, append=True, tbl_name=tbl)
-        table_size_2 = db.db.get_size_of_single_import_table(tbl)
-
+        self.api.prepare_directory_for_import(source_dir=self.import_source, append=True, tbl_name=tbl)
+        table_size_2 = self.api.db.get_size_of_single_import_table(tbl)
 
         self.assertEqual(table_size_2, 156)
-
-        db.cleanup(fast=True)
 
     def test_allowed_ext_override(self):
         """
         Check that if we're passing an empty set for allowed_ext, the imported files will be empty.
         """
-        db = PhotoAPI(root_path=self.temp_db)
-
-        tbl = db.prepare_directory_for_import(source_dir=os.path.join(self.media_source, "db"),
-                                              allowed_ext=set())
+        tbl = self.api.prepare_directory_for_import(source_dir=os.path.join(self.media_source, "db"),
+                                                    allowed_ext=set())
 
         allowed_sum = 0
-        for row in db.db.update_allowed_iterator(tbl):
+        for row in self.api.db.update_allowed_iterator(tbl):
             key, allowed, org_fname = row
             if allowed == Allowed.ALLOWED:
                 allowed_sum += 1
 
         self.assertEqual(0, allowed_sum)
 
-        db.cleanup(fast=True)
-
     def test_purging(self):
         """
         CHeck if purging works correctly.
         """
-        db = PhotoAPI(root_path=self.temp_db)
-
         # Copy the files
         shutil.copytree(os.path.join(self.media_source, "db"), self.import_source)
 
         tbl = "Test_Table"
 
-        db.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
+        self.api.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
 
         # Check the size after the first run
-        self.assertEqual(152, db.db.get_size_of_single_import_table(tbl))
+        self.assertEqual(152, self.api.db.get_size_of_single_import_table(tbl))
 
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
 
         # Number should be the same
-        self.assertEqual(152, db.db.get_size_of_single_import_table(tbl))
+        self.assertEqual(152, self.api.db.get_size_of_single_import_table(tbl))
 
         # Change the directory. New number should be reflected
         shutil.rmtree(self.import_source)
         shutil.copytree(os.path.join(self.media_source, "import_base_dir"), self.import_source)
 
         # With purge, we should end with 4 files
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source, purge=True, tbl_name=tbl)
 
-        self.assertEqual(4, db.db.get_size_of_single_import_table(tbl))
-
-        db.cleanup(fast=True)
+        self.assertEqual(4, self.api.db.get_size_of_single_import_table(tbl))
 
     def test_file_exists(self):
         """
         Check Add file to import table raises an error if a file is encoutered twice
         """
-
-        db = PhotoAPI(root_path=self.temp_db)
         shutil.copytree(os.path.join(self.media_source, "db"), self.import_source)
 
         tbl = "Test_Table"
 
-        db.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl)
+        self.api.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl)
 
         for root, dirs, files in os.walk(self.import_source):
             for f in files:
-                self.assertRaises(ValueError, lambda : db._prepare_file_import(tbl_name=tbl,
-                                                                               allowed_ext=set(),
-                                                                               append=False,
-                                                                               file_path=os.path.join(root, f)))
-
-        db.cleanup(True)
+                self.assertRaises(ValueError, lambda: self.api._prepare_file_import(tbl_name=tbl,
+                                                                                    allowed_ext=set(),
+                                                                                    append=False,
+                                                                                    file_path=os.path.join(root, f)))
 
     def test_recursive(self):
         """
         Check that if we don't search recursively, the db dir, which only contains directories, will not add a single
         row to the import table
         """
-        db = PhotoAPI(root_path=self.temp_db)
         shutil.copytree(os.path.join(self.media_source, "db"), self.import_source)
 
         tbl = "Test_Table"
 
-        db.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl, recursive=False)
+        self.api.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl, recursive=False)
 
-        self.assertEqual(0, db.db.get_size_of_single_import_table(tbl))
+        self.assertEqual(0, self.api.db.get_size_of_single_import_table(tbl))
 
         # Copy directory with 4 files
         for root, _, files in os.walk(os.path.join(self.media_source, "import_base_dir")):
             for f in files:
                 shutil.copy2(os.path.join(root, f), os.path.join(self.import_source, f))
 
-        db.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl, recursive=False, purge=True)
+        self.api.prepare_directory_for_import(source_dir=self.import_source, tbl_name=tbl, recursive=False, purge=True)
 
-        self.assertEqual(4, db.db.get_size_of_single_import_table(tbl))
-
-        db.cleanup(True)
+        self.assertEqual(4, self.api.db.get_size_of_single_import_table(tbl))
 
     def test_really_long_table_name(self):
         """
         Test if the table name will be truncated
         """
-        db = PhotoAPI(root_path=self.temp_db)
         shutil.copytree(os.path.join(self.media_source, "db"), self.import_source)
 
-        very_long_name = "HelloWorld"*15
+        very_long_name = "HelloWorld" * 15
 
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source, tbl_name=very_long_name, recursive=False)
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source, tbl_name=very_long_name,
+                                                    recursive=False)
 
         self.assertEqual(len(tbl), 120)
-
-        db.cleanup(True)
 
     def test_update_allowed(self):
         """
         Check that update allowed works correctly
         """
-        db = PhotoAPI(root_path=self.temp_db)
         shutil.copytree(os.path.join(self.media_source, "import_base_dir"), self.import_source)
 
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source, allowed_ext={".png", ".jpg"})
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source, allowed_ext={".png", ".jpg"})
 
         allowed_sum = 0
-        for row in db.db.update_allowed_iterator(tbl):
+        for row in self.api.db.update_allowed_iterator(tbl):
             key, allowed, org_fname = row
             if allowed == Allowed.ALLOWED:
                 allowed_sum += 1
@@ -319,10 +287,10 @@ class TestAPIPrepareDirectoryForImport(unittest.TestCase):
         self.assertEqual(2, allowed_sum)
 
         # Update, make all files included
-        db.update_allowed(allowed_ext={".png", ".jpg", ".jpeg", ".tiff"}, tbl=tbl)
+        self.api.update_allowed(allowed_ext={".png", ".jpg", ".jpeg", ".tiff"}, tbl=tbl)
 
         allowed_sum = 0
-        for row in db.db.update_allowed_iterator(tbl):
+        for row in self.api.db.update_allowed_iterator(tbl):
             key, allowed, org_fname = row
             if allowed == Allowed.ALLOWED:
                 allowed_sum += 1
@@ -330,26 +298,22 @@ class TestAPIPrepareDirectoryForImport(unittest.TestCase):
         self.assertEqual(4, allowed_sum)
 
         # Update, other half not included
-        db.update_allowed(allowed_ext={".jpeg", ".tiff"}, tbl=tbl)
+        self.api.update_allowed(allowed_ext={".jpeg", ".tiff"}, tbl=tbl)
 
         allowed_sum = 0
-        for row in db.db.update_allowed_iterator(tbl):
+        for row in self.api.db.update_allowed_iterator(tbl):
             key, allowed, org_fname = row
             if allowed == Allowed.ALLOWED:
                 allowed_sum += 1
 
         self.assertEqual(2, allowed_sum)
-        db.cleanup(True)
 
     def test_file_ext_update_allowed(self):
         """
         Check that file exts without . are raise a ValueError
         """
-        db = PhotoAPI(root_path=self.temp_db)
         shutil.copytree(os.path.join(self.media_source, "import_base_dir"), self.import_source)
 
-        tbl = db.prepare_directory_for_import(source_dir=self.import_source, allowed_ext={".png", ".jpg"})
+        tbl = self.api.prepare_directory_for_import(source_dir=self.import_source, allowed_ext={".png", ".jpg"})
 
-        self.assertRaises(ValueError, lambda : db.update_allowed(allowed_ext={"jpg"}, tbl=tbl))
-
-        db.cleanup(True)
+        self.assertRaises(ValueError, lambda: self.api.update_allowed(allowed_ext={"jpg"}, tbl=tbl))
