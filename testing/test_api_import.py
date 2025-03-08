f771361bcb52b2bc1bd201e9886d5faddc58ba72
AliSot2000 <alisot200@gmail.com>
2025-03-08 15:28:03 +0100

    + Added TODO

--- testing/test_api_import.py
+++ testing/test_api_import.py
@@ -12,7 +12,7 @@ from photo_lib.metadata_aggregator import DateTimeSource
 from photo_lib.new_photo_model import PhotoAPI
 from photo_lib.utils import rec_list_all
 
-
+# TODO add full table dump for verification.
 wip: bool = False
 
 class TestAPIPrepareDirectoryForImport(unittest.TestCase):
