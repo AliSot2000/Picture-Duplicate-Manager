7854256a5f1e37da50a1bff2cc7509f3fb9269b2
AliSot2000 <alisot200@gmail.com>
2025-03-09 08:35:53 +0100

    + Added sorting

--- testing/test_api_import.py
+++ testing/test_api_import.py
@@ -987,7 +987,7 @@ class TestAPIPerformImport(unittest.TestCase):
         rel_paths.remove(".photos.db")
 
         if wip:
-            print(json.dumps(rel_paths, indent=4))
+            print(json.dumps(rel_paths.sort(), indent=4))
 
         db_paths = [
             "1990/01/01/1990-01-01T12-00-00_0001.png",
@@ -1164,7 +1164,7 @@ class TestAPIPerformImport(unittest.TestCase):
         files = [p.removeprefix(custom_target_dir).removeprefix(os.sep) for p in rel_paths]
 
         if wip:
-            print(json.dumps(files, indent=4))
+            print(json.dumps(files.sort(), indent=4))
 
         db_files = [
             "1990-01-01T12-00-00_0001.png",
@@ -1340,7 +1340,7 @@ class TestAPIPerformImport(unittest.TestCase):
         files = [p.removeprefix(custom_target_dir).removeprefix(os.sep) for p in rel_paths]
 
         if wip:
-            print(json.dumps(files, indent=4))
+            print(json.dumps(files.sort(), indent=4))
 
         db_files = [
             "1990-01-01T12-00-00_0001_1_1.png",
