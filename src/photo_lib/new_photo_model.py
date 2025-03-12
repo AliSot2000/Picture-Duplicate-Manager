123efe9eb1e20713d353b89c9aa1511d17bb4ccd
AliSot2000 <alisot200@gmail.com>
2025-03-12 18:46:26 +0100

    + Formatting

--- src/photo_lib/new_photo_model.py
+++ src/photo_lib/new_photo_model.py
@@ -2488,6 +2488,7 @@ class PhotoAPI:
             # No need for larger logging info, handled within the internal functions
             suc = self._create_img_thumbnails(
                 in_path=self.db.temp_video_path(), out_path=out_path, major_size=major_size)
+
             assert os.path.exists(self.db.temp_video_path()), "Video file missing despite successfully creating it?"
             os.remove(self.db.temp_video_path())
             return suc
