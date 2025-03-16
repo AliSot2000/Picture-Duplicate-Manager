ab8d5fd26f46c31acb632809c09e8b2c722af6fe
AliSot2000 <alisot200@gmail.com>
2025-03-16 20:47:37 +0100

    + Added imports

--- testing/ui_functions/test_db_classify_functions.py
+++ testing/ui_functions/test_db_classify_functions.py
@@ -1,15 +1,12 @@
+import datetime
 import json
+import os
+import shutil
 import time
-import unittest
 import zoneinfo
 
 from photo_lib.custom_enum import MediaType
-from photo_lib.new_photo_model import PhotoAPI
-from typing import Optional
-import os
-import datetime
-import shutil
-
+from .base_class import TestClassifyBase
 
 """
 This file fully tests the following functions:
