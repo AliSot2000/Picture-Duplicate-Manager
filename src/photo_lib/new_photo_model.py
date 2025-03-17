c0e67538fc9d6cf6974090e36d0932e6a19be349
AliSot2000 <alisot200@gmail.com>
2025-03-17 18:48:47 +0100

    + New dt mandatory

--- src/photo_lib/new_photo_model.py
+++ src/photo_lib/new_photo_model.py
@@ -1965,7 +1965,7 @@ class PhotoAPI:
                             flags: MainFlags,
                             dbn: str,
                             dt: datetime.datetime,
-                            new_datetime: datetime.datetime = None,
+                            new_datetime: datetime.datetime,
                             db_local_dir: str = None):
         """
         Internal function to move a file once its datetime has been updated. Movement needed because resolution of
@@ -1983,8 +1983,6 @@ class PhotoAPI:
         :raises FileNotFoundError: if the path where the file is currently supposed to be doesn't exist
         :raises FileExistsError: if the path where the file is supposed to be moved to does exist
         """
-        new_datetime = dt if new_datetime is None else new_datetime
-
         # Parse the paths.
         if db_local_dir is not None:
             current_path = os.path.join(self.root_path, *self.db.parse_db_local_dir(db_local_dir), dbn)
