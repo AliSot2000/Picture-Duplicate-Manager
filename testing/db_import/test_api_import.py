0f78e155488d45de7b84be79adc8a649ea02fd2a
AliSot2000 <alisot200@gmail.com>
2025-03-18 20:53:02 +0100

    + Using different BaseClass + Added INFOS

--- testing/db_import/test_api_import.py
+++ testing/db_import/test_api_import.py
@@ -279,123 +279,12 @@ class TestAPIPerformImport(PerformImportBaseClass):
     """
     Test all functions surrounding the perform_import method.
     """
-    shadow_db: str
-    temp_db: str
-    media_source: str
-    import_source: str
-
-    api: Optional[PhotoAPI] = None
-    tgt_table: Optional[str] = None
-
-    @classmethod
-    def setUpClass(cls):  # pragma: no cover
-        cls.shadow_db = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "shadow_db"))
-        cls.temp_db = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "test_db"))
-        cls.media_source = os.path.join(os.path.dirname(__file__), "..", "test_file_out")
-        cls.import_source = os.path.join(os.path.dirname(__file__), "..", "scratch")
-        cls.tbl_dump_dir = os.path.join(os.path.dirname(__file__), "..", "db_dump")
-
-        # Check the input files are present
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), "..", "test_file_out")):
-            raise FileNotFoundError(
-                "Need test files to test the db. Create them with the scripts/generate_dummy_media.py"
-            )
-
-        # Check and remove the past of the shadow db
-        if os.path.exists(cls.shadow_db):
-            shutil.rmtree(cls.shadow_db)
-
-        # Create a fresh instance
-        db = PhotoAPI(root_path=cls.shadow_db,
-                      init=True,
-                      init_loggers=True,
-                      opt_integrity_check=True)
-
-        cls.tgt_table = db.prepare_directory_for_import(source_dir=os.path.join(cls.media_source, "db"))
-
-        db.cleanup()
-
-    @classmethod
-    def tearDownClass(cls):  # pragma: no cover
-        # Class teardown is the removing of the shadow db
-        path = cls.shadow_db
-
-        shutil.rmtree(path)
-
-        delattr(cls, "tgt_table")
-        delattr(cls, "shadow_db")
-        delattr(cls, "temp_db")
-        delattr(cls, "media_source")
-        delattr(cls, "import_source")
-        delattr(cls, "tbl_dump_dir")
-
-    def setUp(self):  # pragma: no cover
-        """
-        Setup function creates a fresh instance of the db to run the tests against
-        """
-        # Part of setup is teardown of the test db
-        if os.path.exists(self.temp_db):
-            shutil.rmtree(self.temp_db)
-
-        shutil.copytree(self.shadow_db, self.temp_db)
-
-        # Part of setup is teardown of the import_source directory
-        if os.path.exists(self.import_source):
-            shutil.rmtree(self.import_source)
-
-        self.api = PhotoAPI(root_path=self.temp_db,
-                            init_loggers=False,
-                            init=False)
-        self.api.config.batch_size = 10
-
-    def tearDown(self):  # pragma: no cover
-        """
-        Remove the local instance of the db.
-        """
-        self.api.cleanup(True)
-        self.api = None
-
-        # Part of setup is teardown of the test db
-        if os.path.exists(self.temp_db):
-            shutil.rmtree(self.temp_db)
-
-        # Part of setup is teardown of the import_source directory
-        if os.path.exists(self.import_source):
-            shutil.rmtree(self.import_source)
-
-    # ==================================================================================================================
-    # Actual Test methods
-    # ==================================================================================================================
-
-    def test_internal_dir(self):
-        """
-        Test the internal dir function (checks that a given path is inside the scope of the db)
-        """
-        rel_root = "foo/bar/baz"
-        not_equalchild_of_root = os.path.join(self.import_source, "some_dir")
-        child_file = os.path.join(self.temp_db, "baz.txt")
-
-        os.makedirs(not_equalchild_of_root, exist_ok=True)
-        with open(child_file, "w") as f:
-            f.write("Content")
-
-        child_of_temp = os.path.join(self.api.db.get_temp_dir(), "some_dir")
-        child_of_thumb = os.path.join(self.api.db.get_thumb_dir(), "some_dir")
-        child_of_trash = os.path.join(self.api.db.get_trash_dir(), "some_dir")
-
-        os.makedirs(child_of_temp, exist_ok=True)
-        os.makedirs(child_of_thumb, exist_ok=True)
-        os.makedirs(child_of_trash, exist_ok=True)
-
-        # Check the default problems
-        self.assertRaises(TypeError, lambda: self.api.verify_custom_target_dir(tgt_dir=rel_root))
-        self.assertRaises(TypeError, lambda: self.api.verify_custom_target_dir(tgt_dir=child_file))
-        self.assertRaises(ValueError, lambda: self.api.verify_custom_target_dir(tgt_dir=not_equalchild_of_root))
-
-        # Check the db directories
-        self.assertRaises(ValueError, lambda: self.api.verify_custom_target_dir(tgt_dir=child_of_temp))
-        self.assertRaises(ValueError, lambda: self.api.verify_custom_target_dir(tgt_dir=child_of_thumb))
-        self.assertRaises(ValueError, lambda: self.api.verify_custom_target_dir(tgt_dir=child_of_trash))
+    # INFO: api.verify_custom_target_dir is tested in import_aux_tests.py
+    # INFO: api.db.insert_row_main_table is tested in import_aux_tests.py
+    # INFO: api.db.insert_row_metadata_table is tested in import_aux_tests.py
+    # INFO: api.update_row_main_table is tested in import_aux_tests.py
+    # INFO: api.update_row_metadata_table is tested in import_aux_tests.py
+    # INFO: api.set_imported_status is tested in import_aux_tests.py
 
     def test_correct_paths_rel(self):
         """
