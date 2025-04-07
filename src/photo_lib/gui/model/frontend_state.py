33f739195fb30a87f3c3b73ee1c008de9f434999
AliSot2000 <alisot200@gmail.com>
2025-04-07 11:55:34 +0200

    + Added file for progress state of long-running actions

---
+++ src/photo_lib/gui/model/frontend_state.py
@@ -0,0 +1,20 @@
+import pydantic
+from enum import Enum
+from pydantic import BaseModel
+
+"""
+Contains the progress of an action runner during a long running action
+"""
+
+
+class TaskType(Enum):
+    """
+    List of all known tasks.
+    """
+
+
+class UIState(pydantic.BaseModel):
+    current_task: TaskType
+    progress: str | int
+
+
