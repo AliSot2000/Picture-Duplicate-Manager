2c69a68699c8233494412353ecc56b60e1d985f4
AliSot2000 <alisot200@gmail.com>
2025-04-07 10:41:18 +0200

    + Added a new Exception in case didn't find a key in the table

--- src/photo_lib/errors_and_warnings.py
+++ src/photo_lib/errors_and_warnings.py
@@ -34,3 +34,16 @@ class ImplementationError(Exception):
 
     def __str__(self):
         return repr(self.message)
+
+
+class KeyNotFound(Exception):
+    def __init__(self, table: str, key: int, message: str = None):
+        self.message = message
+        self.tbl = table
+        self.key = key
+
+    def __str__(self):
+        if self.message is not None:
+            return f"Key: {self.key} not in {self.tbl}. {self.message}"
+
+        return f"Key: {self.key} not in {self.tbl}"
