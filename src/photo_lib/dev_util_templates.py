e5a8fec7ebba3cc9b9efd36fba492bdc7599a96a
AliSot2000 <alisot200@gmail.com>
2025-04-08 17:10:03 +0200

    + Added template for dev_util files (Contains some variables for the

---
+++ src/photo_lib/dev_util_templates.py
@@ -0,0 +1,26 @@
+"""
+File contains variables and declarations needed to facilitate development. Please copy this file to dev_util.py
+and populate the variables
+"""
+from photo_lib.data_objects import MediaPaths
+
+# Var provides a path to a library which contains actual data that can be viewed.
+full_scratch_root = "<Fill-This-In>"
+
+
+# Output directory where the files form the generate_dummy_media.py are stored
+synthetic_file_out = "<Fill-This-In>"
+
+
+# Path to where a library populated with the files generated with the generate_dummy_media.py is located
+synthetic_scratch_root = "<Fill-This-In>"
+
+
+# Provide a MediaPaths object with different paths for each scale (without a database to test image widgets)
+different_default_media_paths: MediaPaths = None
+
+
+# Provide a MediaPaths object with the same picture at different sizes (without a database to test image widgets)
+same_default_media_paths: MediaPaths = None
+
+
