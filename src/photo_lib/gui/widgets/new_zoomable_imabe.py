240eda279af66031304c63d6f76d0432ad112791
AliSot2000 <alisot200@gmail.com>
2025-04-08 16:48:06 +0200

    + Removed sample image

--- src/photo_lib/gui/widgets/new_zoomable_imabe.py
+++ src/photo_lib/gui/widgets/new_zoomable_imabe.py
@@ -302,13 +302,3 @@ class ZoomImage(BaseImage):
             r.moveCenter(self.rect().center() + self.__position_offset.toPoint() * (2 ** (1 + self.__scale_offset / 100)))
         qp = QPainter(self)
         qp.drawPixmap(r, self.pixmap)
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ZoomImage()
-    # window.file_path = "/media/alisot2000/DumpStuff/Test128/2022-09-01 02.35.12_001.jpg"
-    window.file_path = "/home/alisot2000/Desktop/2024-30-11-Photos-Retrat/20241130 1722 Boestritt [Alex S] 6.jpg"
-    window.show()
-
-    sys.exit(app.exec())
