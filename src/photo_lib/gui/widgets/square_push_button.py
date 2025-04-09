beecb4bba99f752c3771a2d87e1dd43e11738e3d
AliSot2000 <alisot200@gmail.com>
2025-04-09 16:07:21 +0200

    + Deleted square_push_button.py

--- src/photo_lib/gui/widgets/square_push_button.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QPushButton
-
-
-class QSquarePushButton(QPushButton):
-    """
-    Button that stays square - used for icons.
-    """
-    def heightForWidth(self, a0: int) -> int:
-        return self.width()
