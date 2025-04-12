6fa464d4cd86c38b13806dec1179e6e8fa08668a
AliSot2000 <alisot200@gmail.com>
2025-04-12 17:17:11 +0200

    + Added stub for later

--- src/photo_lib/gui/model/frontend_preferences.py
+++ src/photo_lib/gui/model/frontend_preferences.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -10,4 +12,20 @@ class UIUserPreferences(BaseModel):
     """
     Contains shorthands (basically things the way the user left them so they are picked up correctly again.
     """
-    ...
+    # Preferences for Tiles.
+    tile_size_main_main: Optional[int] = None
+    tile_size_main_trash: Optional[int] = None
+    tile_size_main_duplicates: Optional[int] = None
+    tile_size_main_sel_a: Optional[int] = None
+    tile_size_main_sel_b: Optional[int] = None
+    tile_size_main_verify: Optional[int] = None
+
+    tile_size_import: Optional[int] = None
+
+    tile_size_relocate: Optional[int] = None
+
+    tie_size_hash: Optional[int] = None
+
+    tile_size_name: Optional[int] = None
+
+    tile_size_presence : Optional[int] = None
