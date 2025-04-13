ac116949f1513e3d4164c48674e00caf4befcbe6
AliSot2000 <alisot200@gmail.com>
2025-04-13 23:24:41 +0200

    + Added a widget for the headers in the tile_view

---
+++ src/photo_lib/gui/temp_new_wigets/header_widget.py
@@ -0,0 +1,98 @@
+"""
+File contains a Header which is a QFrame containing a QHBoxLayout with a QCheckBox.
+"""
+from typing import Callable
+
+from PyQt6.QtCore import QRect
+from PyQt6.QtGui import QFont, QRegion
+from PyQt6.QtWidgets import QCheckBox, QFrame, QHBoxLayout, QWidget
+
+from photo_lib.gui.util.fonts import get_h1_font_size
+
+
+class HeaderWidget(QFrame):
+    """
+    Header widget that contains a QCheckBox and a QHBoxLayout.
+    """
+    font_size_getter: Callable[[], int] = get_h1_font_size
+
+    def __init__(self, text: str = None, parent: QWidget = None):
+        """
+        Create a new instance of a HeaderWidget.
+
+        :param text: Text to display in the checkbox.
+        :param parent: Parent widget.
+        """
+        super().__init__(parent)
+        self.setFrameShape(QFrame.Shape.StyledPanel)
+        self.setLineWidth(1)
+
+        self.check_box = QCheckBox()
+
+        if text is not None:
+            self.check_box.setText(text)
+
+        self.check_box.setTristate(True)
+
+        self.basic_layout = QHBoxLayout()
+        self.basic_layout.addWidget(self.check_box)
+
+        self.setLayout(self.basic_layout)
+
+    def set_text(self, text: str, font_size: Callable[[], int] = None):
+        """
+        Set the text of the checkbox.
+
+        :param text: Text to set.
+        :param font_size: A function that returns an int with the font size. I.e. functions in  photo_lib.gui.util.fonts
+        """
+        if font_size is not None:
+            self.font_size_getter = font_size
+            self.update_font()
+
+        self.check_box.setText(text)
+
+    def test(self):
+        """
+        Get the text of the checkbox.
+        """
+        return self.check_box.text()
+
+    def update_font(self):
+        """
+        Update the font i.e. the font size using the font size getter.
+        """
+        font = QFont()
+        font.setPointSize(self.font_size_getter())
+        self.check_box.setFont(font)
+
+    def repaint(self):
+        """
+        Catch repaint event to update the font size with the getter
+        (needed in case the user changes the system font size)
+        """
+        self.update_font()
+        super().repaint()
+
+    def repaint_rect(self, rect: QRect):
+        """
+        Catch repaint event to update the font size with the getter
+        (needed in case the user changes the system font size)
+        """
+        self.update_font()
+        super().repaint(rect)
+
+    def repaint_xywh(self, x: int, y: int, w: int, h: int):
+        """"
+        Catch repaint event to update the font size with the getter
+        (needed in case the user changes the system font size)"""
+        self.update_font()
+        super().repaint(x, y, w, h)
+
+    def repaint_region(self, region: QRegion):
+        """
+        Catch repaint event to update the font size with the getter
+        (needed in case the user changes the system font size)
+        """
+        self.update_font()
+        super().repaint(region)
