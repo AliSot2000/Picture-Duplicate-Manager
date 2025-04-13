e01636e1e8c68e6fd7e528653405277dcfe7124e
AliSot2000 <alisot200@gmail.com>
2025-04-13 13:34:44 +0200

    + Calling update() and updateGeometry() when an image is loaded

--- src/photo_lib/gui/new_main_window.py
+++ src/photo_lib/gui/new_main_window.py
@@ -38,7 +38,8 @@ class BaseMainWindow(QMainWindow):
         result.tgt_widget.pixmap = result.pm
         result.tgt_widget.width_div_height = result.wdh
         result.tgt_widget.file_path = result.file_path
-        result.tgt_widget.repaint()
+        result.tgt_widget.update()
+        result.tgt_widget.updateGeometry()
 
 
 class DeduplicatorMainWindow(BaseMainWindow):
